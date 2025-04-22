6fc58286aebfada6436a1e92dfb1ceee59f76f17
steveSujo <stevuuuuu@gmail.com>
2025-04-22 16:22:11 +0530

    fix(main.rs): better cli app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
     env::{self, Args},
+    fs::{self},
+    io,
     path::PathBuf,
 };
 
@@ -21,23 +23,58 @@ struct Config {
 fn main() {
     let mut program_conf: Config = Config::default();
 
+    //TODO: refactor CLI
     let mut flags: Args = env::args().into_iter();
 
-    while let Some(flag) = flags.next() {
-        match flag.as_str() {
-            "-i" => program_conf.is_interactive = true,
-            "-f" => {
-                if let Some(path) = flags.next() {
-                    program_conf.file_path = PathBuf::from(path);
+    //skip the default exe path arg
+    flags.next();
+
+    if flags.len() >= 1 {
+        while let Some(flag) = flags.next() {
+            match flag.as_str() {
+                "-i" | "--interactive" => program_conf.is_interactive = true,
+                "-f" | "--file" => {
+                    if let Some(path) = flags.next() {
+                        program_conf.file_path = PathBuf::from(path);
+                    }
                 }
+                "-h" | "--help" => {
+                    print_help();
+                    break;
+                }
+                _ => println!("Unknown Argument Passed!"),
             }
-            _ => println!("Unknown Argument Passed!"),
         }
+    } else {
+        //start interactive promtp
+        program_conf.is_interactive = true;
     }
 
+    match program_conf {
+        Config {
+            is_interactive: true,
+            ..
+        } => run_prompt(),
+        Config {
+            is_interactive: false,
+            file_path: path,
+        } => run_file(path),
+    };
+}
+fn print_help() {
     println!(
-        "\nprogram config:\n is_interactive: {}\n file_path: {}",
-        program_conf.is_interactive,
-        program_conf.file_path.display()
-    );
+        "\nUsage: pseudo [Option] \
+             \n\nOptions:\
+              \n\t-h,--help\t\tDisplay this help\
+              \n\t-i,--interactive\tStart interactive Prompt (default if no file is passed)\
+              \n\t-f,--file\t\tInput file to interpret\
+"
+    )
+}
+}
+
+
+
+
+
 }
